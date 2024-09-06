7dd81a947876396549cb24936bf8719e94f48b6b
Bogdan <bogtaran87@yandex.ru>
2024-09-06 17:36:04 +0600

    space

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ print('Hallo, World!')
 x = 5
 print(x)
 
+
 print('1111111')
 print('2222222')
 
--- nums/plus.py
+++ nums/plus.py
@@ -2,6 +2,7 @@ def plus(a, b):
     return a + b
 
 
+
 if __name__ == '__main__':
     print(plus(2, 2))
     print(plus(3, 3))
